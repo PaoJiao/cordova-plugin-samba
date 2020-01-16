a2528e799c4f39d10689629a3753467ea98d3eb0
openflake <app@zerg.cc>
2020-01-16 11:02:46 +0800

    handle screen direction changed

--- src/android/imageview/ImageViewerActivity.java
+++ src/android/imageview/ImageViewerActivity.java
@@ -1,16 +1,19 @@
 package net.cloudseat.smbova;
 
+import android.app.Activity;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+
+import android.content.res.Configuration;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
 
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-
-import android.app.Activity;
 import java.io.IOException;
 import net.cloudseat.smbova.R;
 
@@ -20,6 +23,7 @@ public class ImageViewerActivity extends Activity {
 
     private ImageView imageView;
     private ProgressBar loading;
+    private BitmapCreateTask task;
 
     public interface ImageCreator {
         byte[] getByteArray() throws IOException;
@@ -32,19 +36,30 @@ public class ImageViewerActivity extends Activity {
 
         imageView = (ImageView) findViewById(R.id.image_view);
         loading = (ProgressBar) findViewById(R.id.loading);
-        new BitmapCreateTask().execute(imageCreator);
+
+        task = new BitmapCreateTask();
+        task.execute(imageCreator);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        task.cancel(true);
     }
 
     /**
-     * Android 不允许在主线程请求网络，否则抛出 NetworkOnMainThreadException
-     * 故开启新的任务线程获取图片和更新UI
+     * Android 不允许在主线程（UI线程）请求网络，否则抛出 NetworkOnMainThreadException
+     * 故开启新的任务线程获取图片和更新UI。
      */
     private class BitmapCreateTask extends AsyncTask<ImageCreator, Integer, Bitmap> {
+
+        // 执行任务前回调：在主线程执行，允许更新UI
         @Override
         protected void onPreExecute() {
             loading.setVisibility(View.VISIBLE);
         }
 
+        // 执行任务：在子线程执行，不允许更新UI
         @Override
         protected Bitmap doInBackground(ImageCreator... params) {
             try {
@@ -56,6 +71,7 @@ public class ImageViewerActivity extends Activity {
             }
         }
 
+        // 任务完成后回调：在主线程执行，允许更新UI
         @Override
         protected void onPostExecute(Bitmap bitmap) {
             imageView.setImageBitmap(bitmap);
